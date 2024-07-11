1274b9e5bf541f0a07d8baeb67fac25cf1f9163d
Joakim Gyllenskepp <joakim.gyllenskepp@sparebank1.no>
2024-07-11 16:31:14 +0200

    Fjern Next-css+ikon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <a href="./oppgave" className={styles.card} rel="noopener noreferrer">
         <h2>
-          Oppgave <span>-&gt;</span>
+          Oppgave 1 <span>-&gt;</span>
         </h2>
       </a>
     </main>
